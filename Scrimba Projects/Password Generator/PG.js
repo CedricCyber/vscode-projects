cfde2ad215b8e0120d1af61df1c06dc654959e8a
CedricCyber <104404794+CedricCyber@users.noreply.github.com>
2022-07-03 16:48:50 -0500

    Complete

--- Scrimba Projects/Password Generator/PG.js
+++ Scrimba Projects/Password Generator/PG.js
@@ -8,9 +8,12 @@ const passwordEl1 = document.getElementById("password1")
 const passwordEl2 = document.getElementById("password2")
 const symbolsCheckboxEl = document.getElementById("symbols-checkbox")
 const numbersCheckboxEl = document.getElementById("numbers-checkbox")
-const passwordLengthEl = document.getElementById("length-select")
+//Responsive Password Length function (for loop)
+function x() {
+    let passwordLengthEl = document.getElementById("length-select")
+    return passwordLengthEl.value}
 
-//function
+//Generate Password function
 function generatePassword() {
 //variables to store generated password variables
     let password = ""
@@ -19,43 +22,43 @@ function generatePassword() {
 if (numbersCheckboxEl.checked && symbolsCheckboxEl.checked) {
 
 // for loops (One for each password)
-for (let i = 0; i < passwordLengthEl.value; i++) {
-    let generate = Math.floor(Math.random() * characters.length)
-    password += characters[generate]
+        for (let i = 0; i < x(); i++) {
+            let generate = Math.floor(Math.random() * characters.length)
+            password += characters[generate]
         }
 
-for (let i = 0; i < passwordLengthEl.value; i++) {
-    generate = Math.floor(Math.random() * characters.length)
-    password2 += characters[generate]
+        for (let i = 0; i < x(); i++) {
+            generate = Math.floor(Math.random() * characters.length)
+            password2 += characters[generate]
         }
     }
 // if statement (numbers/no symbols)
-    else if (numbersCheckbox.checked) {   
-        for (let i = 0; i < 15; i++) {
+    else if (numbersCheckboxEl.checked) {   
+        for (let i = 0; i < x(); i++) {
             generate = Math.floor(Math.random() * 52 + 10)
             password += characters[generate]
         }
-        for (let i = 0; i < 15; i++) {
+        for (let i = 0; i < x(); i++) {
             generate = Math.floor(Math.random() * 52 + 10)
             password2 += characters[generate]
         }
     }
-    else if (symbolsCheckbox.checked) {
-        for (let i = 0; i < 15; i++) {
+    else if (symbolsCheckboxEl.checked) {
+        for (let i = 0; i < x(); i++) {
             generate = Math.floor(Math.random() * 52 + 28 )
             password += characters2[generate]
         }
-        for (let i = 0; i < 15; i++) {
+        for (let i = 0; i < x(); i++) {
             generate = Math.floor(Math.random() * 52 + 28 )
             password2 += characters2[generate]
         }
     }
     else { 
-        for (let i = 0; i < 15; i++) {
+        for (let i = 0; i < x(); i++) {
             generate = Math.floor(Math.random() * 52 + 1)
             password += characters[generate]
         }
-        for (let i = 0; i < 15; i++) {
+        for (let i = 0; i < x(); i++) {
             generate = Math.floor(Math.random() * 52 + 1)
             password2 += characters[generate]
         }

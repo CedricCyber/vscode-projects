eeeca4eac0f5bdf14112b8f78af9e424ac285846
CedricCyber <104404794+CedricCyber@users.noreply.github.com>
2022-08-03 19:15:10 -0500

    Initial Commit

---
+++ Personal-Projects/JS-Dice-Game/index.js
@@ -0,0 +1,14 @@
+const player1Score = document.getElementById("player1score");
+const player2Score = document.getElementById("player2score");
+const startGameButton = document.getElementById("start-game");
+const resetButton = document.getElementById("reset-button");
+player1Turn = true;
+
+console.log(player1Score);
+console.log(player2Score);
+console.log(startGameButton);
+console.log(resetButton);
+
+function startGame {
+    startGameButton.style.display = "none"
+}
